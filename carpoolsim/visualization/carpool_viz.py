1fa80be0eb5dcdaa7e59343a1bdeaaba95301215
thefriedbee <diyi93@outlook.com>
2024-03-26 11:53:04 -0500

    add type annotation to carpool_viz.py, remove functions not useful

--- carpoolsim/visualization/carpool_viz.py
+++ carpoolsim/visualization/carpool_viz.py
@@ -1,15 +1,16 @@
 import networkx as nx
-import math
 import matplotlib.pyplot as plt
-from matplotlib.ticker import (
-    MultipleLocator,
-    FormatStrFormatter
-)
 import numpy as np
+import pandas as pd
 
 
 # a function creates the accurate graph representations!
-def create_nx_graph(mat, trips, self_loop=False, undirected=True):
+def create_nx_graph(
+        mat: np.ndarray,
+        trips: pd.DataFrame | pd.Series,  # TODO: check if type annotation is accurate
+        self_loop: bool = False,
+        undirected: bool = True,
+):
     mat = mat.copy()
     if self_loop is False:
         np.fill_diagonal(mat, 0)
@@ -25,59 +26,13 @@ def create_nx_graph(mat, trips, self_loop=False, undirected=True):
     return G
 
 
-# a function for plotting graph (in the correct formula)!
-def plot_graph_simple(G: nx.DiGraph):
-    plt.figure(figsize=(20, 5))
-    # default positions
-    pos = nx.spring_layout(G)
-    nx.draw_networkx_nodes(G, pos, node_size=20, node_color='black', node_shape="x")  # nodes
-    nx.draw_networkx_edges(G, pos, width=1, edge_color='grey')  # edges
-    plt.title('Shareability Network Graph of a Trip Clusters (default layout)')
-    return plt.gca()
-
-
-# a function for plotting graph (in the correct formula)!
-def plot_graph_along_time(G, springY=True, edge_color='grey', edge_alpha=0.7, continued=False, fig=None, ax=None):
-    if not continued:
-        fig, ax = plt.subplots(figsize=(20, 5))
-    # default positions
-    pos = nx.spring_layout(G)
-    pos_y = [pos[i][1] for i in range(len(pos))]
-    pos_x = [n[1]['depart_min'] for i, n in enumerate(G.nodes(data=True))]
-    # node pos random.uniform(0, 1)
-    if springY:
-        dct = {i: (pos_x[i], pos_y[i]) for i in range(len(pos_x))}
-        plt.ylabel('Spring layout on Y axis')
-    else:
-        a = math.pi / 4
-        dct = {i: (pos_x[i], math.sin(a * i)) for i in range(len(pos_x))}
-        plt.ylabel('Use sin value on y axis in turn for visualization')
-    nx.draw_networkx_nodes(G, dct, node_size=20,
-                           node_color='black', node_shape="x", alpha=0.7)  # nodes
-    nx.draw_networkx_edges(G, dct, width=1,
-                           edge_color=edge_color, alpha=edge_alpha)  # edges
-    # add lines for viz
-    for pX in pos_x:
-        plt.axvline(x=pX, ymin=0.0, ymax=1.0, color='grey', lw=0.5, alpha=0.5)
-    plt.title('Shareability Network Graph')
-    plt.xlabel('depart time (hour) (5 min minor ticks)')
-    plt.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
-    ax.tick_params(axis="x", direction="out", which="both", right=False, top=False)
-    ax.tick_params(axis='x', which='minor', bottom=True)
-    ax.xaxis.set_major_locator(MultipleLocator(60))
-
-    ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
-    x_labels = ax.get_xticks()
-    # formatter = FuncFormatter(lambda x_val, tick_pos: "$2^{{+{:.0f}}}$".format(np.log2(x_val)))
-    ax.set_xticklabels([str(int(x / 60)) for x in x_labels])
-    ax.minorticks_on()
-    ax.xaxis.set_minor_locator(MultipleLocator(5))
-    return fig, ax
-
-
 # Post analysis after assigning all trips
 # write a function to regenerate summary histograms for each trip
-def plot_hist(trip_sum, on="time", title_info=None):
+def plot_hist(
+        trip_sum: pd.DataFrame,
+        on: str = "time",
+        title_info: str | None = None
+) -> plt.Axes:
     """
     Given trip summary info, plot histogram for interested variable, which could be time, speed and distance.
     :param trip_sum: trip summary DataFrame table
@@ -110,7 +65,11 @@ def plot_hist(trip_sum, on="time", title_info=None):
 
 
 # plot saved time
-def plot_diff_hist(trip_sum, on="time", title_info=None):
+def plot_diff_hist(
+        trip_sum: pd.DataFrame,
+        on: str = "time",
+        title_info: str | None = None
+) -> plt.Axes:
     """
     Plot the changed amount for between before case and after case.
     :param trip_sum: trip summary DataFrame table
