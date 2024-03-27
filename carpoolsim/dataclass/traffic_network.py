36e336298657c4de484f236aa1f28d7dcc60e069
thefriedbee <diyi93@outlook.com>
2024-03-27 15:49:49 -0500

    move taz to traffic_network.py

--- carpoolsim/dataclass/traffic_network.py
+++ carpoolsim/dataclass/traffic_network.py
@@ -3,7 +3,7 @@ Define the standard schema of network links file
 """
 from dataclasses import dataclass
 
-from shapely import LineString
+from shapely import LineString, Polygon
 
 
 @dataclass
@@ -20,3 +20,20 @@ class TrafficNetworkNode:
     node_id: int
     node_lon: float
     node_lat: float
+
+
+@dataclass
+class TrafficAnalysisZone:
+    taz_id: int
+    geometry: Polygon
+    centroid_lon: float
+    centroid_lat: float
+
+    def convert_to_dict(self):
+        return {
+            "taz_id": self.taz_id,
+            "geometry": self.geometry,
+            "centroid_lon": self.centroid_lon,
+            "centroid_lat": self.centroid_lat,
+        }
+
