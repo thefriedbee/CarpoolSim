a52c538a371e0991fb33dbac516cb8305d70f89d
thefriedbee <diyi93@outlook.com>
2024-03-27 16:14:59 -0500

    work on data interface traffic_network.py

--- carpoolsim/dataclass/traffic_network.py
+++ carpoolsim/dataclass/traffic_network.py
@@ -7,33 +7,50 @@ from shapely import LineString, Polygon
 
 
 @dataclass
-class InputTrafficNetworkLink:
-    node_a: int  # link's starting node id
-    node_b: int  # link's ending node id
+class TrafficNetworkLink:
+    a: int  # link's starting node id
+    b: int  # link's ending node id
     a_b: str  # links id (defined by node_a and node_b)
-    geometry: LineString  # geometry of the link (for visualization purpose)
     distance: float  # travel distance along the link
+    geometry: LineString  # geometry of the link (for visualization purpose)
+
+    def convert_to_dict(self):
+        return {
+            "a": self.a,
+            "b": self.b,
+            "a_b": self.a_b,
+            "distance": self.distance,
+            "geometry": self.geometry
+        }
 
 
 @dataclass
 class TrafficNetworkNode:
-    node_id: int
-    node_lon: float
-    node_lat: float
+    nid: int
+    lon: float
+    lat: float
+    # projected coordinates (need to choose the correct projection)
+    x: float
+    y: float
+
+    def convert_to_dict(self):
+        return {
+            "nid": self.nid,
+            "lon": self.lon,
+            "lat": self.lat,
+            "x": self.x,
+            "y": self.y,
+        }
 
 
 @dataclass
 class TrafficAnalysisZone:
     taz_id: int
     geometry: Polygon
-    centroid_lon: float
-    centroid_lat: float
 
     def convert_to_dict(self):
         return {
             "taz_id": self.taz_id,
-            "geometry": self.geometry,
-            "centroid_lon": self.centroid_lon,
-            "centroid_lat": self.centroid_lat,
+            "geometry": self.geometry
         }
 
