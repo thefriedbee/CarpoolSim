b2cadc5cbdc62b6dbcce43845dfbc9545c60e671
thefriedbee <diyi93@outlook.com>
2024-03-27 11:18:39 -0500

    add data input/output path names

--- carpoolsim/basic_settings.py
+++ carpoolsim/basic_settings.py
@@ -10,33 +10,39 @@ os.environ['project_root'] = Path(__file__).parent.parent.as_posix()
 sys.path.append(os.environ['project_root'])
 
 
-# all data information
-os.environ['project_data'] = os.path.join(
+# data inputs and outputs folder
+os.environ['data_inputs'] = os.path.join(
     os.environ['project_root'], "data_inputs"
 )
 
+os.environ['data_outputs'] = os.path.join(
+    os.environ['project_root'], "data_outputs"
+)
+
+
+# more specific paths to required files
 os.environ['network_links'] = os.path.join(
-    os.environ['project_data'],
+    os.environ['data_inputs'],
     'ABM2020 203K', '2020 nodes with latlon', '2020_nodes_latlon.shp'
 )
 
 os.environ['network_nodes'] = os.path.join(
-    os.environ['project_data'],
+    os.environ['data_inputs'],
     'ABM2020 203K', '2020 links', '2020_links.shp'
 )
 
 os.environ['taz'] = os.path.join(
-    os.environ['project_data'],
+    os.environ['data_inputs'],
     'ABM2020 203K', 'taz', 'TAZ_combine.shp'
 )
 
 os.environ['parking_lots'] = os.path.join(
-    os.environ['project_data'], 'Park_and_Ride_locations',
+    os.environ['data_inputs'], 'Park_and_Ride_locations',
     'Park_and_Ride_locations.shp'
 )
 
 os.environ['trip_demands'] = os.path.join(
-    os.environ['project_data'], 'gt_survey',
+    os.environ['data_inputs'], 'gt_survey',
     '2022_campus_commute_survey_data_no_zip_na_05122023.csv'
 )
 
--- carpoolsim/prepare_input.py
+++ carpoolsim/prepare_input.py
@@ -1,6 +1,32 @@
 """
 Prepare inputs for the analysis...
+
+A traffic network class is defined consists of three parts:
+- network links: directed links of the network
+- network nodes: nodes (end points) of the network
+- network tazs: traffic analysis zones
 """
+
+import geopandas as gpd
+
+from carpoolsim.network_prepare import (
+    initialize_abm15_links,
+    build_carpool_network,
+)
+
+
+class TrafficNetwork:
+    def __init__(
+            self,
+            network_links: gpd.GeoDataFrame,
+            network_nodes: gpd.GeoDataFrame,
+            tazs: gpd.GeoDataFrame,
+    ) -> None:
+        self.network_links = network_links
+        self.network_nodes = network_nodes
+        self.tazs = tazs
+
+    pass
 
 
 
