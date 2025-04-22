143a9f7d0283e48ef713ca57571f83ce59491690
lilyyy01 <yuening.li@vanderbilt.edu>
2025-04-21 21:25:46 -0400

    setup python version lessen restriction to 3.10

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="carpoolsim",
+    version="0.1.1",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    # other metadata...
+)
