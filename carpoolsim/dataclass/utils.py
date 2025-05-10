580912bfde336c2d7a1326ef5b2329e4151fc4d9
lilyyy01 <yuening.li@vanderbilt.edu>
2025-05-10 14:10:15 -0500

    add geospatial processing unit

---
+++ carpoolsim/dataclass/utils.py
@@ -0,0 +1,39 @@
+"""
+functions to expand dataclass to pandas dataframe (geospatial processing)
+"""
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+
+# attach coordinate to the nearest taz centroid node
+def attach_coord2taz_node(df_row, df_nodes, mode="orig"):
+    filt = (df_nodes.nid == str(df_row[f"{mode}_taz"]))
+    X = df_nodes.loc[filt, "x"].iloc[0]
+    Y = df_nodes.loc[filt, "y"].iloc[0]
+    node = df_nodes.loc[filt, "nid"].iloc[0]
+    if mode == "orig":
+        return pd.Series({"ox": X, "oy": Y, "o_node": str(node)})
+    elif mode == "dest":
+        return pd.Series({"dx": X, "dy": Y, "d_node": str(node)})
+    else:
+        raise IOError
+    
+# alias for attach_coord2taz_node
+get_xy = attach_coord2taz_node
+
+
+# for each parking lot, search the corresponding TAZ located in
+def get_contained_taz(x: Point, tazs: gpd.GeoDataFrame):
+    filt = tazs.contains(x)
+    if sum(filt) > 0:
+        taz = tazs.loc[filt, 'taz_id'].iloc[0]
+    else:
+        taz = -1
+    return taz
+
+# alias for get_contained_taz
+get_taz = get_contained_taz
+
+
+
