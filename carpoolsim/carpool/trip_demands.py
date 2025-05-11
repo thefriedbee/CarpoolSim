9b942498bbacff8c7f57eaf473c960947290cdd5
lilyyy01 <yuening.li@vanderbilt.edu>
2025-05-11 14:52:27 -0500

    add infrastructure class

--- carpoolsim/carpool/trip_demands.py
+++ carpoolsim/carpool/trip_demands.py
@@ -12,14 +12,25 @@ from carpoolsim.carpool.util.network_search import (
 )
 
 
+class Infrastructure:
+    def __init__(
+        self, 
+        network: nx.DiGraph, 
+        links: pd.DataFrame, 
+        engine: sqlalchemy.Engine, 
+        parking_lots: pd.DataFrame | None = None
+    ) -> None:
+        self.network = network
+        self.links = links
+        self.engine = engine
+        self.parking_lots = parking_lots
+
+
 class TripDemands:
     def __init__(
         self,
         df: pd.DataFrame,
-        network: nx.DiGraph,
-        links: pd.DataFrame,
-        engine: sqlalchemy.Engine,
-        parking_lots: pd.DataFrame | None = None
+        infrastructure: Infrastructure,
     ) -> None:
         cols_to_drop = [
             'ox_sq', 'oy_sq', 'dx_sq', 'dy_sq',
@@ -32,14 +43,27 @@ class TripDemands:
         self.int2idx = {i: index for i, (index, row) in enumerate(df.iterrows())}
         self.idx2int = {index: i for i, (index, row) in enumerate(df.iterrows())}
         self.num_travelers = len(df)
-        self.network = network
-        self.links = links
-        self.engine = engine
-        self.parking_lots = parking_lots
+        self.infrastructure = infrastructure
         # record travel information
         self.soloPaths = []  # trip retention of SOV paths
         self.soloDists = []  # trip retention of SOV distance
         self.soloTimes = []  # trip retention of SOV time
+    
+    @property
+    def network(self):
+        return self.infrastructure.network
+    
+    @property
+    def links(self):
+        return self.infrastructure.links
+    
+    @property
+    def engine(self):
+        return self.infrastructure.engine
+    
+    @property
+    def parking_lots(self):
+        return self.infrastructure.parking_lots
 
     def compute_sov_info(self) -> None:
         trips = self.trips
